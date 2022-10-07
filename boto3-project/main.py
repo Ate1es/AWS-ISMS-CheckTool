2ead37c8fb71cd7633e90e3831d4d30721e3cf20
Ate1es <45005846+Ate1es@users.noreply.github.com>
2022-10-07 16:23:36 +0900

    Update main.py

--- boto3-project/main.py
+++ boto3-project/main.py
@@ -1,18 +1,21 @@
+# -*- coding: utf-8 -*-
 from os import access
 import boto3
+import csv
 import requests
 from datetime import datetime, timezone
 
+
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+session = boto3.Session()
 
 def utc_to_local(utc_dt):
     return utc_dt.replace(tzinfo=timezone.utc).astimezone(tz=None)
 
 def diff_dates(date1, date2):
     return abs(date2 - date1).days
-
-session = boto3.Session()
 
 def find_iamUser_name_list():
     iam = session.client('iam', verify=False)
@@ -102,14 +105,116 @@ def get_iamRole_lastUsed():
             print(RoleName+" : 한번도 사용된 적 없음")
 
 
+def get_securityGroup_list():
+    cidr_block = ""
+    ip_protocol=""
+    from_port=""
+    to_port=""
+    from_source=""
 
+    f = open('Security_Group.csv','w',encoding='utf-8',newline='')
+    wr = csv.writer(f)
 
+    print("%s, %s, %s, %s, %s, %s, %s" % ("Region", "Group-Name", "Group-ID", "In/Out", "Protocol", "Port", "Source/Destination"))
+
+    wr.writerow(["Region", "Group-Name", "Goup-ID", "In/Out", "Protocol", "Port", "Source","Destination"])
+
+    regions = session.get_available_regions('ec2')
+
+    for region in regions:
+        ec2_client = boto3.client('ec2', region, verify=False)
+        try:
+            vpcs = ec2_client.describe_vpcs()
+            sgs = ec2_client.describe_security_groups()["SecurityGroups"]
+            
+            for sg in sgs:
+                group_name = sg["GroupName"]
+                group_id = sg["GroupId"]
+                inbound = sg["IpPermissions"]
+
+                for rule in inbound:
+                    print(group_name)
+                    print(rule)
+                    if rule["IpProtocol"] == "-1":
+
+                        traffic_type = "All Traffic"
+                        ip_porotocol = "ALL"
+                        to_port = "ALL"
+                        print(region+" --> "+group_name+" : ALL Inbound 정책 사용중 [ 취약 ]")
+
+                    else:
+                        ip_protocol = rule["IpProtocol"]
+                        from_port = rule["FromPort"]
+                        to_port = rule["ToPort"]
+                        if to_port == -1:
+                            to_port = "N/A"
+                    
+                    if len(rule["IpRanges"]) > 0:
+                        for ip_range in rule["IpRanges"]:
+                            cidr_block = ip_range["CidrIp"]
+                            if "Description" in ip_range.keys():
+                                desc = ip_range["Description"]
+                                wr.writerow([region,group_name,group_id,"Inbound",ip_protocol,to_port,cidr_block,desc])
+                            else: wr.writerow([region,group_name,group_id,"Inbound",ip_protocol,to_port,cidr_block])
+                    
+                    if len(rule["Ipv6Ranges"]) > 0:
+                        for ip_range in rule["Ipv6Ranges"]:
+                            cidr_block = ip_porotocol_range["CidrIpv6"]
+                            if "Description" in ip_range.keys():
+                                desc = ip_range["Description"]
+                                wr.writerow([region,group_name,group_id,"Inbound",ip_protocol,to_port,cidr_block,desc])
+                            else: wr.writerow([region,group_name,group_id,"Inbound",ip_protocol,to_port,cidr_block])
+
+                    if len(rule["UserIdGroupPairs"]) > 0:
+                        for source in rule["UserIdGroupPairs"]:
+                            from_source = source["GroupId"]
+                            wr.writerow([region,group_name,group_id,"Inbound",ip_protocol,to_port,from_source])
+
+                outbound = sg["IpPermissionsEgress"]
+                for rule in outbound:
+                    if rule["IpProtocol"] == "-1":
+                        traffic_type = "All Trafic"
+                        ip_protocol = "ALL"
+                        to_port="ALL"
+                    else:
+                        ip_protocol = rule["IpProtocol"]
+                        from_port = rule["FromPort"]
+                        to_port = rule["ToPort"]
+                        if to_port == -1:
+                            to_port = "N/A"
+                    
+                    if len(rule["IpRanges"]) > 0:
+                        for ip_range in rule["IpRanges"]:
+                            cidr_block = ip_range["CidrIp"]
+                            if "Description" in ip_range.keys():
+                                desc = ip_range["Description"]
+                                wr.writerow([region,group_name,group_id,"Outbound",ip_protocol,to_port,cidr_block,desc])
+                            else: wr.writerow([region,group_name,group_id,"Inbound",ip_protocol,to_port,cidr_block])
+
+                    if len(rule["Ipv6Ranges"]) > 0:
+                        for ip_range in rule["Ipv6Ranges"]:
+                            cidr_block = ip_range["CidrIpv6"]
+                            if "Description" in ip_range.keys():
+                                desc = ip_range["Description"]
+                                wr.writerow([region,group_name,group_id,"Outbound",ip_protocol,to_port,cidr_block,desc])
+                            else: wr.writerow([region,group_name,group_id,"Inbound",ip_protocol,to_port,cidr_block])
+
+                    if len(rule["UserIdGroupPairs"]) > 0:
+                        for source in rule["UserIdGroupPairs"]:
+                            from_source = source["GroupId"]
+                            wr.writerow([region,group_name,group_id,"Outbound",ip_protocol,to_port,from_source])
+
+        except Exception as e:
+            print(region+" is Inactivated")
+            
+    f.close()
 
 if __name__=='__main__':
-    get_user_last_password_used()
-    print()
-    get_user_active_key_age_and_lastUsed()
+    #get_user_last_password_used()
+    #print()
+    #get_user_active_key_age_and_lastUsed()
     #print()
     #get_user_mfa() 미완성
-    print()
-    get_iamRole_lastUsed()
+    #print()
+    #get_iamRole_lastUsed()
+    get_securityGroup_list()
